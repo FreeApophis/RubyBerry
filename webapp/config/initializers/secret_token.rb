e5325b08117502fe48d142ab2d5ea49438247569
Thomas Bruderer <apophis@apophis.ch>
2014-04-27 14:22:48 +0200

    Bare Rails Application (pi-control)

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PiControl::Application.config.secret_key_base = '5673832e3619f183a7de3410b9ee17b89d0924d1bd4a6a23c4fc73d2f0ab175c02ea1c1dd7872833a918a567629928c97be38dfe1f333a39310456498e1ed7ad'
---
+++ webapp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PiControl::Application.config.session_store :cookie_store, key: '_pi-control_session'
